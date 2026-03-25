aeba1e01306c34f04fc44e3decb2383d05b3d683
yooseohyeon <yooseohyeon.02@gmail.com>
2026-03-25 17:35:13 +0900

    refactor: TodoList 내의 개별 아이템 렌더링 로직을 TodoItem 컴포넌트로 분리

---
+++ src/components/TodoItem.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export const TodoItem = ({ todo, toggleTodo }) => {
+  return (
+    <div className={todo.done ? "todo-item done" : "todo-item"}>
+      <input
+        type="checkbox"
+        className="todo-checkbox"
+        checked={todo.done}
+        onChange={() => toggleTodo(todo.id)}
+      />
+      <span className={todo.done ? "todo-title done" : "todo-title"}>
+        {todo.title}
+      </span>
+      {/* TODO 8: 상세 페이지로 이동하는 Link를 만드세요 */}
+      <Link to={`/todos/${todo.id}`} style={{ textDecoration: "none" }}>
+        <span style={{ color: "#2196f3", fontSize: "14px" }}>상세</span>
+      </Link>
+    </div>
+  );
+};
--- src/pages/TodoList.jsx
+++ src/pages/TodoList.jsx
@@ -1,4 +1,5 @@
-import { useSearchParams, Link } from "react-router-dom";
+import { useSearchParams } from "react-router-dom";
+import { TodoItem } from "../components/TodoItem";
 
 const TodoList = ({ todos, toggleTodo }) => {
   // TODO 5: useSearchParams로 URL 검색 파라미터를 관리하세요
@@ -35,24 +36,7 @@ const TodoList = ({ todos, toggleTodo }) => {
         <p className="empty">검색 결과가 없습니다.</p>
       ) : (
         filteredTodos.map((todo) => (
-          <div
-            key={todo.id}
-            className={todo.done ? "todo-item done" : "todo-item"}
-          >
-            <input
-              type="checkbox"
-              className="todo-checkbox"
-              checked={todo.done}
-              onChange={() => toggleTodo(todo.id)}
-            />
-            <span className={todo.done ? "todo-title done" : "todo-title"}>
-              {todo.title}
-            </span>
-            {/* TODO 8: 상세 페이지로 이동하는 Link를 만드세요 */}
-            <Link to={`/todos/${todo.id}`} style={{ textDecoration: "none" }}>
-              <span style={{ color: "#2196f3", fontSize: "14px" }}>상세</span>
-            </Link>
-          </div>
+          <TodoItem key={todo.id} todo={todo} toggleTodo={toggleTodo} />
         ))
       )}
     </div>
