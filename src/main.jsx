01c41868fb6f5eef06c06f706061c6eed4211f29
yooseohyeon <yooseohyeon.02@gmail.com>
2026-03-25 17:22:11 +0900

    chore: React Router v7 future flags 설정 추가

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,12 @@ import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter
+      future={{
+        v7_startTransition: true,
+        v7_relativeSplatPath: true,
+      }}
+    >
       <App />
     </BrowserRouter>
   </StrictMode>,
